4b0a004387ca5c74ca313f25ced3db856741ffda
SasukeDaTony <tonytaylor@pursuit.org>
2024-01-10 12:16:29 -0500

    env file with portname specified added

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
 const app = require("./app");
 
-app.listen(9000, () => console.log("app is running on 9000!"));
+require("dotenv").config();
+
+app.listen(process.env.PORT, () => console.log(`app is running on 7040!`));
